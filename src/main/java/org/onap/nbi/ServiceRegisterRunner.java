d74ef2eab4f15c0ed4c3bfdbf83b2e0ddd7c4cc9
Quoc Nghia Nguyen <quocnghia.nguyen@orange.com>
2018-05-16 17:19:55 +0200

    (WIP) Register with MSB once fully launched
    /!\ FOR PREVIEW PURPOSE ONLY /!\
    
    Testing guide:
    
      1. Build the jar
          `mvn clean package`
      2. Launch the MSB stack
          `docker-compose -f docker-compose.msb.yml up msb_consul msb_discovery msb_apigateway`
      3. Launch 3 NBI instances and the rest of the stack
          `docker-compose -f docker-compose.msb.yml up --build --scale nbi=3 nbi`
      4. By calling the endpoint http://localhost/api/microservices/v1/services,
         verify that the NBI service has been registered with 3 nodes (status=PASSING)
      5. By calling the endpoint http://localhost/api/nbi/v1/status,
         verify that it is load balanced
      6. Scale down the nbi service, 1 node will be killed
          `docker-compose -f docker-compose.msb.yml scale nbi=2`
      7. Verify that the dead node's status is now CRITICAL
    
    Change-Id: I2169e7b0686e60bbe4e9eba69d4b3f72411217d2
    Issue-ID: EXTAPI-90
    Signed-off-by: Quoc Nghia Nguyen <quocnghia.nguyen@orange.com>

---
+++ src/main/java/org/onap/nbi/ServiceRegisterRunner.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2018 Orange
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.nbi;
+
+import com.google.common.base.Strings;
+import org.onap.msb.sdk.discovery.entity.MicroServiceInfo;
+import org.onap.msb.sdk.discovery.entity.Node;
+import org.onap.msb.sdk.httpclient.msb.MSBServiceClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Register this NBI instance with the MSB when the app is fully started
+ */
+@Component
+public class ServiceRegisterRunner implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(ServiceRegisterRunner.class);
+
+    @Value("${msb.discovery.host}")
+    private String DISCOVERY_HOST;
+
+    @Value("${msb.discovery.port}")
+    private int DISCOVERY_PORT;
+
+    @Value("${msb.service.host}")
+    private String SERVICE_HOST;
+
+    @Value("${msb.service.port}")
+    private String SERVICE_PORT;
+
+    @Value("${msb.service.name}")
+    private String SERVICE_NAME;
+
+    @Value("${msb.service.version}")
+    private String SERVICE_VERSION;
+
+    @Value("${msb.service.url}")
+    private String SERVICE_URL;
+
+    @Value("${msb.service.custom_path}")
+    private String SERVICE_CUSTOM_PATH;
+
+    @Value("${msb.service.protocol}")
+    private String SERVICE_PROTOCOL;
+
+    @Value("${msb.service.visual_range}")
+    private String SERVICE_VISUAL_RANGE;
+
+    @Value("${msb.service.enable_ssl}")
+    private boolean SERVICE_ENABLE_SSL;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        logDebug();
+
+        MicroServiceInfo msinfo = new MicroServiceInfo();
+        msinfo.setServiceName(SERVICE_NAME);
+        msinfo.setVersion(SERVICE_VERSION);
+        msinfo.setUrl(SERVICE_URL);
+        msinfo.setProtocol(SERVICE_PROTOCOL);
+        msinfo.setVisualRange(SERVICE_VISUAL_RANGE);
+        msinfo.setEnable_ssl(SERVICE_ENABLE_SSL);
+
+        if (!Strings.isNullOrEmpty(SERVICE_CUSTOM_PATH)) {
+            msinfo.setPath(SERVICE_CUSTOM_PATH);
+        }
+
+        Set<Node> nodes = new HashSet<>();
+        Node thisNode = new Node();
+        thisNode.setIp(Strings.isNullOrEmpty(SERVICE_HOST) ? InetAddress.getLocalHost().getHostAddress() : SERVICE_HOST);
+        thisNode.setPort(SERVICE_PORT);
+        thisNode.setCheckType("HTTP");
+        thisNode.setCheckUrl(SERVICE_URL + "/status");
+        nodes.add(thisNode);
+        msinfo.setNodes(nodes);
+
+        MSBServiceClient msbClient = new MSBServiceClient(DISCOVERY_HOST, DISCOVERY_PORT);
+        msbClient.registerMicroServiceInfo(msinfo);
+    }
+
+    private void logDebug() {
+        logger.debug(
+                "Register this service with msb discovery (" + DISCOVERY_HOST + ":" + DISCOVERY_PORT + "):\n"
+                        + " - host: [" + SERVICE_HOST + "]\n"
+                        + " - port: [" + SERVICE_PORT + "]\n"
+                        + " - name: [" + SERVICE_NAME + "]\n"
+                        + " - version: [" + SERVICE_VERSION + "]\n"
+                        + " - url: [" + SERVICE_URL + "]\n"
+                        + " - path: [" + SERVICE_CUSTOM_PATH + "]\n"
+                        + " - protocol: [" + SERVICE_PROTOCOL + "]\n"
+                        + " - visualRange: [" + SERVICE_VISUAL_RANGE + "]\n"
+                        + " - enableSSL: [" + SERVICE_ENABLE_SSL + "]\n"
+        );
+    }
+}
