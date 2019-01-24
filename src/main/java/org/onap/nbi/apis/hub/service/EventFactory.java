ef2285ce29450b7ebe14525199fd0766ea44b335
romaingimbert <romain.gimbert@orange.com>
2019-01-24 11:01:03 +0100

    hub resource format date
    -fix code
    -add listener for testing
    -add karate test
    
    Change-Id: Ife2d7812c77316b75c55e86056f59d838e117069
    Issue-ID: EXTAPI-180
    Signed-off-by: romaingimbert <romain.gimbert@orange.com>

--- src/main/java/org/onap/nbi/apis/hub/service/EventFactory.java
+++ src/main/java/org/onap/nbi/apis/hub/service/EventFactory.java
@@ -1,17 +1,14 @@
 /**
- *     Copyright (c) 2018 Orange
+ * Copyright (c) 2018 Orange
  *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.onap.nbi.apis.hub.service;
 
@@ -19,15 +16,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.MappingJsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 import org.onap.nbi.apis.hub.model.Event;
 import org.onap.nbi.apis.hub.model.EventType;
 import org.onap.nbi.apis.serviceorder.model.ServiceOrder;
 import org.onap.nbi.apis.serviceorder.model.ServiceOrderItem;
 import org.onap.nbi.commons.JacksonFilter;
 import org.onap.nbi.commons.JsonRepresentation;
-
-import java.util.Date;
-import java.util.UUID;
 
 public class EventFactory {
 
@@ -39,11 +37,14 @@ public class EventFactory {
         event.setEventDate(new Date());
         event.setEventType(eventType.value());
 
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        mapper.setDateFormat(df);
+
         JsonNode serviceOrderJson = mapper.valueToTree(filterServiceOrder(serviceOrder));
 
         if (EventType.SERVICE_ORDER_ITEM_STATE_CHANGE.equals(eventType)) {
             JsonNode serviceOrderItemJson = mapper.valueToTree(serviceOrderItem);
-            ((ObjectNode)serviceOrderJson).putArray("orderItem").add(serviceOrderItemJson);
+            ((ObjectNode) serviceOrderJson).putArray("orderItem").add(serviceOrderItemJson);
         }
 
         event.setEvent(serviceOrderJson);
--- src/main/java/org/onap/nbi/apis/hub/service/NotifierService.java
+++ src/main/java/org/onap/nbi/apis/hub/service/NotifierService.java
@@ -1,35 +1,31 @@
 /**
- *     Copyright (c) 2018 Orange
+ * Copyright (c) 2018 Orange
  *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package org.onap.nbi.apis.hub.service;
 
+import javax.validation.Valid;
 import org.onap.nbi.apis.hub.model.Event;
 import org.onap.nbi.apis.hub.model.Subscriber;
+import org.onap.nbi.exceptions.BackendFunctionalException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import javax.validation.Valid;
-
 @Service
 public class NotifierService {
+
     private final Logger logger = LoggerFactory.getLogger(NotifierService.class);
 
     @Autowired
@@ -37,7 +33,12 @@ public class NotifierService {
 
     @Async
     public void run(Subscriber subscriber, @Valid Event event) {
-        ResponseEntity<String> re = restTemplate.postForEntity(subscriber.getCallback(), event, String.class);
-        if (re.getStatusCode() == HttpStatus.OK) logger.debug("FAILED");
+        try {
+            restTemplate.postForEntity(subscriber.getCallback(), event, Object.class);
+        } catch (BackendFunctionalException e) {
+            logger.error(" unable to post event to {} , receive {}, {}", subscriber.getCallback(), e.getHttpStatus(),
+                e.getBodyResponse());
+        }
+
     }
 }
